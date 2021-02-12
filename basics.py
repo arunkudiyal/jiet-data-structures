19c7f015c6a06ad9034256eccc3076d222cbe6e5
Arun Kudiyal <arunkudiyal@icloud.com>
2021-02-12 18:13:52 +0530

    Code Updated

---
+++ basics.py
@@ -0,0 +1,39 @@
+
+# Data types
+# int, String, list, tuples, dictionaries, sets
+
+
+output = 0x10       # type = ? 
+print(output)       # output = ?
+
+i = 3+2j
+print(i)        
+
+
+# str = 'Python is a very(' stupid language'
+str1 = 'hello\world'
+print(str1)
+
+str2 = ''' "Hello this is me" '''
+
+# (), (a), (a,b)
+for i in range(0, 10, 2):
+    print(i)
+
+
+
+numbers = [10, 20, 30, 40, "Arun"]
+
+
+phonebook = {
+    "adam": 123,
+    "eve": 456,
+    "steve": 789
+}
+
+# print the number of eve
+print()
+
+# to create a set of 10 number 0-10 with their cube values
+
+cubes = { x: x*x*x  for x in range(11) }
